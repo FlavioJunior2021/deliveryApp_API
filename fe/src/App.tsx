145535c7c98a05528a2e733978aaafaaeff3c1e7
Flavio Junior <flavio.junior@escolar.ifrn.edu.br>
2023-02-18 11:14:52 -0300

    inicialize project

---
+++ fe/src/App.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable @typescript-eslint/no-extra-semi */		
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable linebreak-style */
+
+export function App(){
+  return <h1>Hello world</h1>;
+};
