e4bc62a6456db19330b97c3708d98c7dd344a915
Flavio Junior <flavio.junior@escolar.ifrn.edu.br>
2023-02-22 10:44:04 -0300

    create Menu component

--- app/App.tsx
+++ app/App.tsx
@@ -2,7 +2,10 @@ import { useFonts } from 'expo-font';
 import { Main } from './src/Main';
 import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+export default function App(): JSX.Element {
 	const [isFontsLoaded] = useFonts({
 		'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
 		'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
